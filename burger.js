9702cd49a227261ba7d2a7e241b9f7c239eaa5d7
ekaterinavdeyuk <ekaterinavdeyuk@gmail.com>
2020-04-10 23:00:26 +0300

    update

--- burger.js
+++ burger.js
@@ -1,66 +1,106 @@
 class Burger {
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = stuffing;
+    constructor() {
+        this.size = 0;
+        this.stuffing = [];
+        this.sauce = '';
         this.price = []
     }
 
     getSize() {
         let smallSize = document.querySelector('.button-small');
-        let toppingSize = document.querySelector('.topping');
+        let toppingSize = document.querySelectorAll('.topping');
         let bigSize = document.querySelector('.button-big');
         let burgerSize = document.querySelectorAll('.burger-size');
         smallSize.addEventListener('click', () => {
             burgerSize.forEach(element => element.style.width = '200px');
-            toppingSize.style.width = '210px';
+            toppingSize.forEach(element => element.style.width = '210px');
             this.size = 'smallSize';
             this.getPrice();
         });
         bigSize.addEventListener('click', () => {
             burgerSize.forEach(element => element.style.width = '300px');
-            toppingSize.style.width = '310px';
+            toppingSize.forEach(element => element.style.width = '320px');
             this.size = 'bigSize';
+            this.getPrice();
+        });
+    }
+
+    addTopping() {
+        let cheese = document.querySelector('.cheese');
+        let salad = document.querySelector('.salad');
+        let bacon = document.querySelector('.bacon');
+        let cheeseButton = document.getElementById('cheese');
+        let saladButton = document.getElementById('salad');
+        let baconButton = document.getElementById('bacon');
+
+        cheeseButton.addEventListener('click', () => {
+            cheese.classList.add('get-topping');
+            cheese.style.background = '#F7CF24';
+            this.stuffing[0] = 'cheese';
+            this.getPrice();
+        });
+
+        saladButton.addEventListener('click', () => {
+            salad.classList.add('get-topping');
+            salad.style.background = '#72C901';
+            this.stuffing[1] = 'salad';
+            this.getPrice();
+        });
+
+        baconButton.addEventListener('click', () => {
+            bacon.classList.add('get-topping');
+            bacon.style.background = '#D9855B';
+            this.stuffing[2] = 'bacon';
+            this.getPrice();
+        });
+    }
+
+    addSauce() {
+        let mustardButton = document.querySelector('.mustard');
+        let ketchupButton = document.querySelector('.ketchup');
+        mustardButton.addEventListener('click', () => {
+            this.sauce = 'mustard';
+            this.getPrice();
+        });
+        ketchupButton.addEventListener('click', () => {
+            this.sauce = 'ketchup';
             this.getPrice();
         })
     }
 
-    addTopping() {
-        let topping = document.querySelector('.topping');
+    deleteTopping() {
+        this.addTopping();
         let cheese = document.querySelector('.cheese');
         let salad = document.querySelector('.salad');
-        let potato = document.querySelector('.potato');
-        cheese.addEventListener('click', () => {
-            topping.classList.add('get-topping');
-            topping.style.background = '#FAFF65';
-            this.stuffing = 'cheese';
-            this.getPrice();
-        });
-        salad.addEventListener('click', () => {
-            topping.classList.add('get-topping');
-            topping.style.background = '#72C901';
-            this.stuffing = 'salad';
-            this.getPrice();
-        });
-        potato.addEventListener('click', () => {
-            topping.classList.add('get-topping');
-            topping.style.background = '#BD7800';
-            this.stuffing = 'potato';
-            this.getPrice();
+        let bacon = document.querySelector('.bacon');
+        let deleteButton = document.querySelector('.delete');
+        deleteButton.addEventListener('click', () => {
+            switch (this.stuffing.pop()) {
+                case 'cheese':
+                    cheese.classList.remove('get-topping');
+                    break;
+                case 'salad':
+                    salad.classList.remove('get-topping');
+                    break;
+                case 'bacon':
+                    bacon.classList.remove('get-topping');
+                    break;
+            }
         });
     }
 
     getPrice() {
         console.log(burger);
-        let price = document.querySelector('.price');
+        let totalPrice = document.querySelector('.price');
         switch (this.stuffing) {
             case 'cheese':
-                this.price[0] = 10;
+                this.price[0] = 50;
                 break;
-            case 'potato':
-                this.price[0] = 15;
+            case 'bacon':
+                this.price[0] = 65;
                 break;
             case 'salad':
-                this.price[0] = 20;
+                this.price[0] = 55;
                 break;
         }
         switch (this.size) {
@@ -71,12 +111,22 @@ class Burger {
                 this.price[1] = 100;
                 break;
         }
-        price.innerText = `Итого: ${this.price.reduce((accumulator, currentValue) =>
+        switch (this.sauce) {
+            case 'mustard':
+                this.price[2] = 15;
+                break;
+            case 'ketchup':
+                this.price[2] = 10;
+                break;
+        }
+        totalPrice.innerText = `Итого: ${this.price.reduce((accumulator, currentValue) =>
             accumulator + currentValue, 0)}руб`;
     }
 }
 
-const burger = new Burger(0 , 0);
+const burger = new Burger();
 burger.getSize();
 burger.addTopping();
 burger.getPrice();
+burger.addSauce();
+burger.deleteTopping();
